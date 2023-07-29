e59987b1f3f2b2eea6d7bc8adee8de292b5ff86b
Utkarsh <utkarshsingh341@gmail.com>
2023-07-29 20:42:26 +0530

    Create Question11.js

---
+++ Question11.js
@@ -0,0 +1,14 @@
+const numbers = [1, 2, 3, 4, 5];
+
+const result = numbers.reduce((acc, num) => {
+  if (num % 2 === 0) {
+    acc.evens.push(num);
+  } else {
+    acc.odds.push(num);
+  }
+  return acc;
+}, { odds: [], evens: [] }); 
+
+console.log(result);
+
+//Explaination in README

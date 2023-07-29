fcdd55022a1898f8edc86f9a943c71762174fe53
Utkarsh <utkarshsingh341@gmail.com>
2023-07-29 20:44:05 +0530

    Update Question7.js

--- Question7.js
+++ Question7.js
@@ -1,7 +1,7 @@
 const arr = [1, 2, 3, 4, 5];
 
-const reversedArr = arr.reduce((acc, num) => {
+const reversedArray = arr.reduce((acc, num) => {
   return [num, ...acc];
 }, []);
 
-console.log(reversedArr);
+console.log(reversedArray);
